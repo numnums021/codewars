0e1cbeaa5dbd0b42bb820040eed1d0a9625c7ad5
danya <numnums021@gmail.com>
2022-07-07 19:55:53 +0300

    ;)

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+;
+test228.iml,1\1\117a763676c1dde16ce2c489d32dc04dfead7e38
+M
+src/com/company/kyu/Kyu6.java,e\a\eae6acc7d6d93022ad670d282aa2069891d261b6
+M
+src/com/company/kyu/Kyu7.java,0\0\00ffd4da3a5608705720f0346146ec218f470ee0
+M
+src/com/company/kyu/Kyu8.java,4\d\4de5d7eac243dd2b35545f37c100dd53be03d1f2
--- src/com/company/kyu/Kyu5.java
+++ src/com/company/kyu/Kyu5.java
@@ -20,4 +20,22 @@ public class Kyu5 {
         if (openBracket == 0) return true;
         return openBracket == closeBracket;
     }
+
+    /*
+    * The Fibonacci numbers are the numbers in the following integer sequence (Fn).
+    * Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+    * F(n) * F(n+1) = prod.
+    * */
+    public static long[] productFib(long prod) {
+        long firstNum = 0, secondNum = 1;
+        while (firstNum * secondNum <= prod) {
+            if ((firstNum * secondNum) == prod) {
+                return new long[]{firstNum, secondNum, 1};
+            }
+            long tmp = firstNum;
+            firstNum = secondNum;
+            secondNum += tmp;
+        }
+        return new long[]{firstNum, secondNum, 0};
+    }
 }
