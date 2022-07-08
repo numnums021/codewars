e21a3c39eae8991a3b78739c3ab0e05ca6b2cc5a
danya <numnums021@gmail.com>
2022-07-08 08:39:52 +0300

    ;)

--- src/com/company/kyu/Kyu6.java
+++ src/com/company/kyu/Kyu6.java
@@ -37,4 +37,20 @@ public class Kyu6 {
         return -1;
     }
 
+    /*You probably know the "like" system from Facebook and other pages.
+    People can "like" blog posts, pictures or other items.
+    We want to create the text that should be displayed next to such an item.
+
+    Implement the function which takes an array containing the names of people that like an item.
+    It must return the display text as shown in the examples:*/
+    public static String whoLikesIt(String... names) {
+        switch (names.length) {
+            case 0: return "no one likes this";
+            case 1: return String.format("%s likes this", names[0]);
+            case 2: return String.format("%s and %s like this", names[0], names[1]);
+            case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        }
+    }
+
 }
