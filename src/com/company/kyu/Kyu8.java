6585abaaaa3daf18b97e7fc19ef59808630832b2
numnums021 <numnums021@gmail.com>
2024-02-24 22:34:53 +0300

    add pillars

--- src/com/company/kyu/Kyu8.java
+++ src/com/company/kyu/Kyu8.java
@@ -4,13 +4,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Kyu8 {
+
+    /*
+      There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same.
+      Your function accepts three arguments:
+      - number of pillars (≥ 1);
+      - distance between pillars (10 - 30 meters);
+      - width of the pillar (10 - 50 centimeters).
+      - Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+    */
+    public int pillars(int numPill, int dist, int width){
+        // Расстояние между первым и последним столбом без учёта их ширины
+        if (numPill <= 1) return 0;
+        if (dist < 10 || dist > 30) return 0;
+        if (width < 10 || width > 50) return 0;
+
+        return (numPill - 1) * dist * 100 + (numPill-2) * width;
+    }
+
     // Implement a function which multiplies two numbers.
     public int multiply(int n1, int n2){
         return n1 * n2;
     }
 
-    public static String buildString(String... args)
-    {
+    public static String buildString(String... args) {
         String answer = "I like ";
         for (int i = 0; i < args.length; i++) {
             if (i != args.length)
